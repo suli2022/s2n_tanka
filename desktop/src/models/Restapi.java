deeb8bdc845d2df165b3d577ed86c86cb2bbfe1b
Sallai András <joska@zold.and>
2023-02-13 09:39:00 +0100

    Kezdés

---
+++ desktop/src/models/Restapi.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Restapi {
+
+    public Restapi() {
+    }
+    
+}
---
+++ desktop/src/views/MainConsole.java
@@ -0,0 +1,8 @@
+package views;
+
+public class MainConsole {
+
+    public MainConsole() {
+    }
+    
+}
